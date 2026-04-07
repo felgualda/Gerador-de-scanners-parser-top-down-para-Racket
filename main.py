264361a51332267f34bcfd5052734324acfc131d
Felipe  Vieira Gualda Pereira <felipepereira_estag@intranet.leste>
2026-04-07 18:59:09 -0300

    Testes do gerador de AFN

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from scanner_gen.leitor_regex import LeitorRegex
 
-r = LeitorRegex.revelar_operador("(a|bc\+)d")
-print(r)
-res = LeitorRegex.shunting_yard(r)
-print(res)
+r1 = LeitorRegex.revelar_operador("(a|bc)d")
+#print(r1)
+r2 = LeitorRegex.shunting_yard(r1)
+#print(r2)
+r3 = LeitorRegex.thompson_construction(r2)
+#print(r3)
+
+r3.visualizar()
